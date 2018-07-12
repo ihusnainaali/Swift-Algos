c91b2fb3ef092f372510c1046a92317dfd9ab139
Prashant Dwivedi <dwi.pra24@gmail.com>
2018-07-12 18:07:44 +0530

    Added most common name finding algo

---
+++ mostCommonName.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+func mostCommonNameFromGivenArray(array:[String]) -> String {
+    var nameCountDictionary = [String : Int]()
+    for name in array {
+        if let count = nameCountDictionary[name] {
+            nameCountDictionary[name] = count+1
+        }else {
+            nameCountDictionary[name] = 1
+        }
+    }
+    
+    var mostCommonName = ""
+    for key in nameCountDictionary.keys {
+        if mostCommonName == "" {
+            mostCommonName = key
+        }else {
+            let count = nameCountDictionary[key]!
+            if count > nameCountDictionary[mostCommonName]! {
+                mostCommonName = key
+            }
+        }
+        print("\(key) : \(nameCountDictionary[key]!)")
+    }
+    return mostCommonName
+}
+print("Most Common Name: ", mostCommonNameFromGivenArray(array: ["bheema", "brajesh", "bheema", "ankit", "bheema", "manoj", "prashant", "manoj", "bheema"]))
