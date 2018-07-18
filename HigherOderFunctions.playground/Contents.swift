f20658c4c625f8b261c6c32787773c7c430e882f
Prashant Dwivedi <dwi.pra24@gmail.com>
2018-07-18 20:45:59 +0530

    added .sorted function

--- HigherOderFunctions.playground/Contents.swift
+++ HigherOderFunctions.playground/Contents.swift
@@ -1,5 +1,17 @@
-//: Playground - noun: a place where people can play
+// =================================================== Higher Order Functions =================================================
 
-import UIKit
+// Sorted function
 
-var str = "Hello, playground"
+let numbers = [1, 3, 3, 5, 7, 2, 8, 4, 6, 6]
+let sortedNumbers = numbers.sorted()
+print(sortedNumbers)
+
+let sortedByDecending = numbers.sorted(by: {$0 > $1})
+print(sortedByDecending)
+
+let decendingSort = numbers.sorted(by: >)
+print(decendingSort)
+
+let ascendingSort = numbers.sorted(by: <)
+print(ascendingSort)
+
